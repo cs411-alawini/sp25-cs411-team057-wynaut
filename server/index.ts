3075d40b952469348b9baf170e2871d5870c5bd6
Bananapad987 <109549589+Bananapad987@users.noreply.github.com>
2025-04-06 13:23:12 -0500

    Front end starter code

---
+++ server/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+
+app.get('/api/',
+    (req: Request, res: Response) => {
+        res.send('API');
+    });
+
+app.listen(PORT, 
+    ()=>{
+        console.log(`Server running on localhost:${PORT}`);
+    })
