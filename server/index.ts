8444528aea9fd823c5a5d209b76c48ed6a3bc919
ddai20 <ddai20@illinois.edu>
2025-04-06 15:53:02 -0500

    connects to server and does an example query on items

--- server/index.ts
+++ server/index.ts
@@ -1,33 +1,42 @@
 import express, { Request, Response } from "express";
 import mysql from "mysql2";
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: "34.132.149.78",
   user: "root",
   password: "",
   database: "Wynaut-Database",
 });
 
-connection.connect((err) => {
-  if (err) {
-    throw err;
-  } else {
-    console.log("Connected!");
-  }
-});
+// connection.connect((err) => {
+//   if (err) {
+//     throw err;
+//   } else {
+//     console.log("Connected!");
+//   }
+// });
 
 const app = express();
 const PORT = 3001;
+const VM_IP = "35.206.80.119";
 
 app.use(express.json());
 app.use(express.urlencoded());
 
 app.use(express.json());
 
-app.get("/api/", (req: Request, res: Response) => {
-  res.send("API");
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, VM_IP, () => {
   console.log(`Server running on localhost:${PORT}`);
 });
+
+connection.query(
+  "SELECT * FROM Items LIMIT 10;",
+  (err: any, results: any, fields: any) => {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  }
+);
