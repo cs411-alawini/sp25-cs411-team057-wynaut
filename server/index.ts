480ac90ffc1227a1f3697eccf715693678c6dd00
ddai20 <ddai20@illinois.edu>
2025-04-06 15:11:49 -0500

    please work

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,33 @@
 import express, { Request, Response } from "express";
+import mysql from "mysql2";
+
+const connection = mysql.createConnection({
+  host: "34.132.149.78",
+  user: "root",
+  password: "",
+  database: "Wynaut-Database",
+});
+
+connection.connect((err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log("Connected!");
+  }
+});
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
+app.use(express.urlencoded());
 
-app.get('/api/',
-    (req: Request, res: Response) => {
-        res.send('API');
-    });
+app.use(express.json());
 
-app.listen(PORT, 
-    ()=>{
-        console.log(`Server running on localhost:${PORT}`);
-    })
+app.get("/api/", (req: Request, res: Response) => {
+  res.send("API");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on localhost:${PORT}`);
+});
