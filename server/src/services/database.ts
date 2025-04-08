07622c99e121d24d50b8478749aaa62c62ef6dce
Kevin Liu <kevinliu5005@gmail.com>
2025-04-08 13:32:43 -0500

    work

---
+++ server/src/services/database.ts
@@ -0,0 +1,11 @@
+import { Receipts } from '../models/Receipts';
+import { Items } from '../models/Items';
+import {receiptsData, itemsData} from '../../../data/mockData';
+import pool from './connections';
+import { RowDataPacket } from "mysql2";
+
+//const receipt: Receipts[] = receiptsData
+//const item: Items[] = itemsData
+
+
+
