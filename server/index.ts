9e975630b764ce309b9088a7275ff48c6774c105
Bananapad987 <109549589+Bananapad987@users.noreply.github.com>
2025-04-08 14:47:37 -0500

    Front end demo

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,15 @@
 import express, { Request, Response } from "express";
-import * as relation from "./models"
+import * as relation from "./models";
 
 const app = express();
-const PORT : number = 3001;
+const PORT: number = 3001;
 
 app.use(express.json());
 
-app.get('/api/',
-    (req: Request, res: Response) => {
-        res.send('API');
-    });
+app.get("/api/", (req: Request, res: Response) => {
+    res.send("API");
+});
 
-app.listen(PORT, 
-    ()=>{
-        console.log(`Server running on localhost:${PORT}`);
-    })
+app.listen(PORT, () => {
+    console.log(`Server running on localhost:${PORT}`);
+});
