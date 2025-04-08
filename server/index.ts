5fd77d9c26412576853583d91a87ae3715c36111
ddai20 <ddai20@illinois.edu>
2025-04-08 17:26:32 -0500

    cors enabled

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,25 @@
 import express, { Request, Response } from "express";
-import * as relation from "./models";
+import bodyParser, { json } from "body-parser";
+import axios from "axios";
+const cors = require("cors");
 
 const app = express();
 const PORT = 3001;
 
-app.use(express.json());
-app.use(express.urlencoded());
-
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/api/", (req: Request, res: Response) => {
     res.send("API");
+});
+
+app.get("/AddReceipt", (req: Request, res: Response) => {
+    res.send("hello");
+});
+
+app.put("/AddReceipt", (req: Request, res: Response) => {
+    console.log(req.body);
+    res.send("accepted");
 });
 
 app.listen(PORT, () => {
