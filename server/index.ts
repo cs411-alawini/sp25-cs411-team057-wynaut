df98df0516af0839ae47c91cd7a0a8d4b321d05a
ddai20 <ddai20@illinois.edu>
2025-04-10 01:48:30 -0500

    working addReceipts

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from "express";
 import bodyParser, { json } from "body-parser";
-import axios from "axios";
+import "./src/services/database";
+import { addItem, addReceipt } from "./src/services/database";
+import { Items, Receipts } from "./src/models/models";
+import pool from "./src/services/connections";
+const date = require("date");
 const cors = require("cors");
 
 const app = express();
@@ -18,8 +22,44 @@ app.get("/AddReceipt", (req: Request, res: Response) => {
 });
 
 app.put("/AddReceipt", (req: Request, res: Response) => {
-    console.log(req.body);
-    res.send("accepted");
+    let currentdate = new Date();
+    let datetime =
+        currentdate.getFullYear() +
+        "-" +
+        (currentdate.getMonth() + 1) +
+        "-" +
+        currentdate.getDate();
+
+    let new_receipt: Receipts = {
+        ReceiptID: 0,
+        UserID: req.body[1],
+        PurchaseDate: datetime,
+        Seller: "none",
+    };
+    addReceipt(new_receipt).then(function (value: number) {
+        let new_receipt_id = value;
+
+        function loop(i: number): void {
+            if (i >= req.body[0].length) {
+                return;
+            }
+
+            let x = req.body[0][i];
+
+            let new_item: Items = {
+                ItemId: 0,
+                ItemName: x["name"],
+                Category: null,
+                ReceiptID: new_receipt_id,
+                Price: x["price"],
+            };
+
+            addItem(new_item).then(() => loop(i + 1));
+        }
+
+        loop(0);
+        res.send("accepted");
+    });
 });
 
 app.listen(PORT, () => {
--- server/src/services/database.ts
+++ server/src/services/database.ts
@@ -1,78 +1,104 @@
-import { Receipts, Items, Test } from '../models/models';
-import pool from './connections';
+import { Receipts, Items, Test } from "../models/models";
+import pool from "./connections";
 import { RowDataPacket } from "mysql2";
 
 export async function getAllReceipts(userID: number): Promise<Receipts[]> {
     const sqlQuery = `Select * From Receipts Where UserID = ${userID};`;
-    const[rows] = await pool.query(sqlQuery);
+    const [rows] = await pool.query(sqlQuery);
     return rows as Receipts[];
 }
 
-export async function addReceipt(receipt: Omit<Receipts, 'ReceiptID'>): Promise<void>{
-    const [maxNum] = await pool.query('Select Max(ReceiptID) as maxNum From Receipts');
-    const nextNum = ((maxNum as [{maxNum:number}])[0].maxNum) + 1;
+export async function addReceipt(
+    receipt: Omit<Receipts, "ReceiptID">
+): Promise<number> {
+    const [maxNum] = await pool.query(
+        "Select Max(ReceiptID) as maxNum From Receipts"
+    );
+    const nextNum = (maxNum as [{ maxNum: number }])[0].maxNum + 1;
     const sqlQuery = `Insert Into Receipts(ReceiptID, UserID, PurchaseDate, Seller) VALUES (${nextNum}, ${receipt.UserID}, '${receipt.PurchaseDate}', '${receipt.Seller}');`;
     await pool.query(sqlQuery);
-    return;
+    return nextNum;
 }
 
-export async function addItem(item: Omit<Items, 'ItemId'>): Promise<void>{
-    const [maxNum] = await pool.query('Select Max(ItemID) as maxNum From Items');
-    const nextNum = ((maxNum as [{maxNum:number}])[0].maxNum) + 1;
-    const sqlQuery = `Insert Into Items(ItemID, Category, ReceiptID, ItemName, Price) VALUES (${nextNum}, '${item.Category}', ${item.ReceiptID}, '${item.ItemName}', ${item.Price});`;
+export async function addItem(item: Omit<Items, "ItemId">): Promise<number> {
+    const [maxNum] = await pool.query(
+        "Select Max(ItemID) as maxNum From Items"
+    );
+    const nextNum = (maxNum as [{ maxNum: number }])[0].maxNum + 1;
+    const sqlQuery = `Insert Into Items(ItemID, Category, ReceiptID, ItemName, Price) VALUES (${nextNum}, ${item.Category}, ${item.ReceiptID}, '${item.ItemName}', ${item.Price});`;
     await pool.query(sqlQuery);
-    return;
+    return nextNum;
 }
 
-export async function deleteReceipt(receiptID: number): Promise<void>{
+export async function deleteReceipt(receiptID: number): Promise<void> {
     const sqlQuery = `Delete From Receipts Where ReceiptID = ${receiptID};`;
     await pool.query(sqlQuery);
     return;
 }
 
-export async function deleteItem(itemID: number): Promise<void>{
+export async function deleteItem(itemID: number): Promise<void> {
     const sqlQuery = `Delete From Items Where ItemID = ${itemID};`;
     await pool.query(sqlQuery);
     return;
 }
 
-export async function updateReceipt(receipt: Receipts): Promise<void>{
+export async function updateReceipt(receipt: Receipts): Promise<void> {
     const sqlQuery = `Update Receipts Set UserID = ${receipt.UserID}, PurchaseDate = '${receipt.PurchaseDate}', Seller = '${receipt.Seller}' Where ReceiptID = ${receipt.ReceiptID};`;
     await pool.query(sqlQuery);
     return;
 }
 
-export async function updateItem(item: Items): Promise<void>{
+export async function updateItem(item: Items): Promise<void> {
     const sqlQuery = `Update Items Set Category = '${item.Category}', ItemName = '${item.ItemName}', Price = ${item.Price} Where ItemID = ${item.ItemId};`;
     await pool.query(sqlQuery);
     return;
 }
 
-export async function getReceipt(receiptID: number): Promise<Receipts>{
+export async function getReceipt(receiptID: number): Promise<Receipts> {
     const sqlQuery = `Select * From Receipts Where ReceiptID = ${receiptID};`;
     const [rows] = await pool.query(sqlQuery);
     return (rows as Receipts[])[0];
 }
 
-export async function getItem(itemID: number):Promise<Items>{
+export async function getItem(itemID: number): Promise<Items> {
     const sqlQuery = `Select * From Items Where ItemID = ${itemID};`;
     const [rows] = await pool.query(sqlQuery);
     return (rows as Items[])[0];
 }
 
 // Testing
-async function main(){
-    await addReceipt({UserID:1, PurchaseDate:"2025-04-09", Seller:"TestSell"});
-    await addItem({Category:"Education", ReceiptID:1, ItemName:"TestItem", Price:1.01});
-    getReceipt(1000).then((results) =>{
+async function main() {
+    await addReceipt({
+        UserID: 1,
+        PurchaseDate: "2025-04-09",
+        Seller: "TestSell",
+    });
+    await addItem({
+        Category: "Education",
+        ReceiptID: 1,
+        ItemName: "TestItem",
+        Price: 1.01,
+    });
+    getReceipt(1000).then((results) => {
         console.log(results);
     });
     getItem(9901).then((results) => {
         console.log(results);
     });
-    await updateReceipt({ReceiptID: 1000, UserID:1, PurchaseDate:"2025-04-09", Seller:"TestSell2"});
-    await updateItem({ItemId:9901, Category:"Education", ReceiptID:1, ItemName:"TestItem2", Price:1.01});
-    getReceipt(1000).then((results) =>{
+    await updateReceipt({
+        ReceiptID: 1000,
+        UserID: 1,
+        PurchaseDate: "2025-04-09",
+        Seller: "TestSell2",
+    });
+    await updateItem({
+        ItemId: 9901,
+        Category: "Education",
+        ReceiptID: 1,
+        ItemName: "TestItem2",
+        Price: 1.01,
+    });
+    getReceipt(1000).then((results) => {
         console.log(results);
     });
     getItem(9901).then((results) => {
