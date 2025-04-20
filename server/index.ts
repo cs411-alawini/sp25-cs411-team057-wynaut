639fe0e4136776b31c020f5c41596a231e3a99c7
ddai20 <ddai20@illinois.edu>
2025-04-20 18:29:19 -0500

    login works

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import bodyParser, { json } from "body-parser";
 import "./src/services/database";
-import { addItem, addReceipt } from "./src/services/database";
+import { addItem, addReceipt, login } from "./src/services/database";
 import { Items, Receipts } from "./src/models/models";
 import pool from "./src/services/connections";
 const date = require("date");
@@ -13,15 +13,15 @@ const PORT = 3001;
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/api/", (req: Request, res: Response) => {
-    res.send("API");
+app.post("/login", (req, res) => {
+    // console.log(req.body);
+    login(req.body["username"], req.body["password"]).then(function (value) {
+        // console.log(`id: ${value} \n`);
+        res.send(value);
+    });
 });
 
-app.get("/AddReceipt", (req: Request, res: Response) => {
-    res.send("hello");
-});
-
-app.put("/AddReceipt", (req: Request, res: Response) => {
+app.put("/addReceipt", (req: Request, res: Response) => {
     let currentdate = new Date();
     let datetime =
         currentdate.getFullYear() +
@@ -36,15 +36,12 @@ app.put("/AddReceipt", (req: Request, res: Response) => {
         PurchaseDate: datetime,
         Seller: "none",
     };
-    addReceipt(new_receipt).then(function (value: number) {
-        let new_receipt_id = value;
 
-        function loop(i: number): void {
-            if (i >= req.body[0].length) {
-                return;
-            }
-
+    addReceipt(new_receipt).then(function (new_receipt_id) {
+        for (let i = 0; i < req.body[0].length; i++) {
             let x = req.body[0][i];
+
+            console.log(x);
 
             let new_item: Items = {
                 ItemId: 0,
@@ -54,11 +51,14 @@ app.put("/AddReceipt", (req: Request, res: Response) => {
                 Price: x["price"],
             };
 
-            addItem(new_item).then(() => loop(i + 1));
+            for (let j = 0; j < x["amount"]; j++) {
+                addItem(new_item);
+            }
         }
 
-        loop(0);
         res.send("accepted");
+
+        console.log(new_receipt_id);
     });
 });
 
